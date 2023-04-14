4c3d20fc83d8642999b7f792650328b389ca963f
naodeng <nao.deng@thoughtworks.com>
2023-04-14 11:04:07 +0800

    first commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/nao.deng/Personal/MyBlog/docusaurus-demo/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/nao.deng/Personal/MyBlog/docusaurus-demo/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/nao.deng/Personal/MyBlog/docusaurus-demo/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/nao.deng/Personal/MyBlog/docusaurus-demo/src/css/custom.css'),
+];
